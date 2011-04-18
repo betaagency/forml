26aa562341c1293ec0a451a5134f39cd3b905fad
Aleksei Kaminin <zendzirou@gmail.com>
2011-04-18 14:49:15 +0400

    Added first level of form scaffolding

---
+++ lib/tag.php
@@ -0,0 +1,76 @@
+<?php
+
+class Tag{
+	static $short_tags = array('img', 'input');
+	var $name = '';
+	function __construct($name){
+		$this->name = $name;
+	}
+
+	static function __callStatic($name, $args){
+		$tag = new Tag($name);
+		if(reset($args))
+			$tag->content(reset($args));
+		return $tag;
+	}
+
+	var $attrs = array();
+	var $content = '';
+	var $before = '';
+	var $after = '';
+	function __call($name, $args){
+		if($name == 'content'){
+			$this->content = $args[0];
+		}elseif($name == 'before'){
+			$this->before = $args[0];
+		}elseif($name == 'after'){
+			$this->after = $args[0];
+		}elseif($name == 'wrap'){
+			$this->before = $args[0];
+			$this->after = $args[1];
+		}elseif(preg_match('/_and_/', $name)){
+			$attrs = explode('_and_', $name);
+			foreach($attrs as $v)
+				$this->{$v}($args[0]);
+		}elseif(preg_match('/_/', $name)){
+			$attrs = explode('_', $name);
+			foreach($attrs as $k=>$v)
+				$this->{$v}($args[$k]);
+		}else{
+			$this->attrs[$name] = $args[0];
+		}
+
+		return $this;
+	}
+
+	function is_short_tag(){
+		if(in_array($this->name, self::$short_tags))
+			return true;
+	}
+
+	function tag_attrs(){
+		if(!$this->attrs)
+			return '';
+		$a = array();
+		foreach($this->attrs as $k=>$v)
+			$a[] = $k.'="'.self::escape($v).'"';
+		return ' '.implode(' ', $a);
+	}
+
+	static function escape($v){
+		return str_replace(array('&','"',"'",'<','>'),
+				   array('&amp;','&quot;','&#39;','&lt;','&gt;'),
+				   $v);
+	}
+
+	function __toString(){
+		$r = '';
+		$r .= $this->before;
+		$r .= "<".$this->name.$this->tag_attrs().">";
+		$r .= $this->content;
+		if(!$this->is_short_tag())
+			$r .= "</".$this->name.">";
+		$r .= $this->after;
+		return $r;
+	}
+}
--- tests/TagTests.php
+++ tests/TagTests.php
@@ -3,4 +3,46 @@ class TagTests extends PHPUnit_Framework_TestCase{
 	static function setUpBeforeClass(){
 		//nothing
 	}
+
+	function test_basic(){
+		$this->assertEquals((string)Tag::p(), '<p></p>');
+	}
+
+	function test_basic_with_content(){
+		$this->assertEquals((string)Tag::p("Hello!"), '<p>Hello!</p>');
+	}
+
+	function test_attributes(){
+		$this->assertEquals((string)Tag::p("Hello!")->class('text'), '<p class="text">Hello!</p>');
+	}
+
+	function test_attribute_quote(){
+		$this->assertEquals((string)Tag::p("Hello!")->title('& hello \' " < >'),
+				    '<p title="&amp; hello &#39; &quot; &lt; &gt;">Hello!</p>');
+	}
+
+	function test_multi_assign_attrs(){
+		$this->assertEquals((string)Tag::input()->id_and_name('firstname'),
+				    '<input id="firstname" name="firstname">');
+	}
+
+	function test_multi_assign_attrs_without_and(){
+		$this->assertEquals((string)Tag::input()->id_name('firstname','name'),
+				    '<input id="firstname" name="name">');
+	}
+
+	function test_img_tag(){
+		$this->assertEquals((string)Tag::img()->src('http://ya.ru/logo.jpg'),
+				    '<img src="http://ya.ru/logo.jpg">');
+	}
+
+	function test_before_and_after(){
+		$this->assertEquals((string)Tag::p('test')->before('-')->after('+'),
+				    '-<p>test</p>+');
+	}
+
+	function test_wrap(){
+		$this->assertEquals((string)Tag::p('test')->wrap('-', '+'),
+				    '-<p>test</p>+');
+	}
 }
